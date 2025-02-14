525efda6cc8f619ddf5ab43b61ef17027668298c
FabioFabrega <fabiofabrega746@gmail.com>
2025-02-14 10:55:21 +0100

    Clase 2025/02/14

--- src/alumnos/alumnos.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { AlumnosService } from './alumnos.service';
-
-@Controller('alumnos')
-export class AlumnosController {
-  constructor(private readonly alumnosService: AlumnosService) {}
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,15 @@ import { UsuarioModule } from './usuario/usuario.module';
 import { BibliotecaModule } from './biblioteca/biblioteca.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PeliculasModule } from './peliculas/peliculas.module';
-import { ProductsModule } from './products/products.module';
+import { Producto } from './_1casoN_N/producto/entities/producto.entity';
+import { ProductoModule } from './_1casoN_N/producto/producto.module';
+import { ProfileModule } from './_caso1_1/profile/profile.module';
+import { UserModule } from './_caso1_1/user/user.module';
+import { UsersModule } from './_caso1_N/users/users.module';
+import { TallaModule } from './_1casoN_N/talla/talla.module';
+import { ProductotallaModule } from './_1casoN_N/productotalla/productotalla.module';
+import { Producto2Module } from './_2casoN_N/producto2/producto2.module';
+import { Talla2Module } from './_2casoN_N/talla2/talla2.module';
 
 @Module({
   imports: [ConfigModule.forRoot({isGlobal:true,}), 
@@ -35,7 +43,15 @@ import { ProductsModule } from './products/products.module';
     BibliotecaModule,
     PokemonModule,
     PeliculasModule,
-    ProductsModule
+    UsersModule,
+    UserModule,
+    ProfileModule,
+    ProductoModule,
+    TallaModule,
+    ProductotallaModule,
+    Producto2Module,
+    Talla2Module
+
   ],
   controllers: [],
   providers: [],
