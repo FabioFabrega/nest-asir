1fcdd15251da147f7bb2592e0092415b8192e48a
FabioFabrega <fabiofabrega746@gmail.com>
2025-02-21 17:01:25 +0100

    limpieza

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,9 @@
 import { Module} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsuarioModule } from './usuario/usuario.module';
-import { BibliotecaModule } from './biblioteca/biblioteca.module';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { PeliculasModule } from './peliculas/peliculas.module';
-import { Producto } from './_1casoN_N/producto/entities/producto.entity';
 import { ProductoModule } from './_1casoN_N/producto/producto.module';
-import { ProfileModule } from './_caso1_1/profile/profile.module';
-import { UserModule } from './_caso1_1/user/user.module';
 import { UsersModule } from './_caso1_N/users/users.module';
 import { TallaModule } from './_1casoN_N/talla/talla.module';
 import { ProductotallaModule } from './_1casoN_N/productotalla/productotalla.module';
@@ -39,13 +34,9 @@ import { Talla2Module } from './_2casoN_N/talla2/talla2.module';
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize:true
     }),
-    UsuarioModule,
-    BibliotecaModule,
     PokemonModule,
     PeliculasModule,
     UsersModule,
-    UserModule,
-    ProfileModule,
     ProductoModule,
     TallaModule,
     ProductotallaModule,
